af430047c0fd357915e2149cc729fae38e34547d
aizigao <weixiangzhe@live.com>
2025-11-30 23:17:47 +0800

    feat: add outline support

---
+++ languages/less/outline.scm
@@ -0,0 +1,90 @@
+;; Less outline
+;; === Comments as annotations (optional)
+(comment) @annotation
+(js_comment) @annotation
+
+;; ===  base cass
+(stylesheet
+    (import_statement
+        "@import" @context
+        ((string_value) @name)) @item)
+
+(rule_set
+    (selectors
+      .
+      (_) @name
+      ("," @name (_) @name)*
+    )) @item
+
+(media_statement
+    "@media" @context
+    (_) @name
+    (block)
+) @item
+
+
+;; === Import statements: @import "xxx.less" / @import url("xxx.css")
+(import_statement
+  "@import" @context
+  (string_value) @name
+) @item
+
+(import_statement
+  "@import" @context
+  (call_expression
+    (function_name) @context
+    (arguments (string_value) @name)
+  )
+) @item
+
+;; === Rulesets: .a, #b { ... } (supports nested selectors)
+(rule_set
+  (selectors
+    (class_selector (class_name) @name)
+    (id_selector (id_name) @name)
+    (descendant_selector
+      (class_selector (class_name) @name)
+      (id_selector (id_name) @name)
+    )*
+  )
+) @item
+
+;; === Supports queries: @supports ... { ... }
+(supports_statement
+  "@supports" @context
+  (feature_query (feature_name) @name (plain_value) @context)
+  (block)
+) @item
+
+;; === Keyframes: @keyframes spin { ... }
+(keyframes_statement
+  "@keyframes" @context
+  (keyframes_name) @name
+  (keyframe_block_list)
+) @item
+
+;; === Mixin definitions: .mixin(...) { ... }
+(mixin_definition
+  (class_name) @name
+  (parameters) @context
+) @item
+
+;; === Plugin statements: @plugin "xxx" / @plugin grid
+(plugin_statement
+  "@plugin" @context
+  (string_value) @name
+) @item
+
+;; === Each statements: each(@colors, { ... })
+(each_statement
+  "each" @context
+  (variable) @name
+  (block)
+) @item
+
+;; 9. Variable declarations: @var: value;
+(declaration
+  (property_name) @name
+  ; _ @context
+  (#match? @name "^@")
+) @item
